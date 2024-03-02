126d00e840f25ca2fe244b239798c902201721b8
Le Duc Thai <leducthai2001@gmail.com>
2024-03-02 22:18:53 +0700

    create gateway signin route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
+import { signIn } from '@gateway/controllers/auth/signin';
 import { signUp } from '@gateway/controllers/auth/signup';
 import { Router } from 'express';
 
 const authRouter = Router();
 
 authRouter.post('/auth/signup', signUp);
+
+authRouter.post('/auth/signin', signIn);
 
 export default authRouter;
