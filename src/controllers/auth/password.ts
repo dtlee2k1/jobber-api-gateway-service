a113e1d1280310c87e514a84300a8ce0970fb452
Le Duc Thai <leducthai2001@gmail.com>
2024-03-03 11:30:06 +0700

    handle gateway forgot-password reset-password and change-password routes

---
+++ src/controllers/auth/password.ts
@@ -0,0 +1,27 @@
+import { authService } from '@gateway/services/api/auth.service';
+import { NextFunction, Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export async function forgotPassword(req: Request, res: Response, _next: NextFunction) {
+  const response = await authService.forgotPassword(req.body.email);
+  res.status(StatusCodes.OK).send({
+    message: response.data.message
+  });
+}
+
+export async function resetPassword(req: Request, res: Response, _next: NextFunction) {
+  const { password, confirmPassword } = req.body;
+  const { token } = req.params;
+  const response = await authService.resetPassword(password, confirmPassword, token);
+  res.status(StatusCodes.OK).send({
+    message: response.data.message
+  });
+}
+
+export async function changePassword(req: Request, res: Response, _next: NextFunction) {
+  const { currentPassword, newPassword } = req.body;
+  const response = await authService.changePassword(currentPassword, newPassword);
+  res.status(StatusCodes.OK).send({
+    message: response.data.message
+  });
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,4 @@
+import { changePassword, forgotPassword, resetPassword } from '@gateway/controllers/auth/password';
 import { signIn } from '@gateway/controllers/auth/signin';
 import { signUp } from '@gateway/controllers/auth/signup';
 import { updateVerifyToken } from '@gateway/controllers/auth/verify-email';
@@ -10,5 +11,11 @@ authRouter.post('/auth/signup', signUp);
 authRouter.post('/auth/signin', signIn);
 
 authRouter.put('/auth/verify-email', updateVerifyToken);
+
+authRouter.put('/auth/forgot-password', forgotPassword);
+
+authRouter.put('/auth//reset-password/:token', resetPassword);
+
+authRouter.put('/change-password', changePassword);
 
 export default authRouter;
