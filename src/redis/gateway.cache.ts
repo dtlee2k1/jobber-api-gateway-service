cf039d6012cd33e636b53ce3cb4fd740e5ba9d95
Le Duc Thai <leducthai2001@gmail.com>
2024-03-17 18:11:51 +0700

    handle gateway cache methods

---
+++ src/redis/gateway.cache.ts
@@ -0,0 +1,74 @@
+import { winstonLogger } from '@dtlee2k1/jobber-shared';
+import { envConfig } from '@gateway/config';
+import { createClient } from 'redis';
+
+type RedisClient = ReturnType<typeof createClient>;
+const logger = winstonLogger(`${envConfig.ELASTIC_SEARCH_URL}`, 'gatewayCache', 'debug');
+
+class GatewayCache {
+  client: RedisClient;
+
+  constructor() {
+    this.client = createClient({ url: `${envConfig.REDIS_HOST}` });
+  }
+
+  public async saveUserSelectedGigCategory(key: string, value: string) {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      await this.client.SET(key, value);
+    } catch (error) {
+      logger.log('error', 'GatewayService cache saveUserSelectedGigCategory() method error:', error);
+    }
+  }
+
+  public async saveLoggedInUserToCache(key: string, value: string) {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const index: number | null = await this.client.LPOS(key, value);
+      if (index === null) {
+        await this.client.LPUSH(key, value);
+        logger.info(`User ${value} added`);
+      }
+      const response: string[] = await this.client.LRANGE(key, 0, -1);
+      return response;
+    } catch (error) {
+      logger.log('error', 'GatewayService Cache saveLoggedInUserToCache() method error:', error);
+      return [];
+    }
+  }
+
+  public async getLoggedInUsersFromCache(key: string) {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response: string[] = await this.client.LRANGE(key, 0, -1);
+      return response;
+    } catch (error) {
+      logger.log('error', 'GatewayService Cache getLoggedInUsersFromCache() method error:', error);
+      return [];
+    }
+  }
+
+  public async removeLoggedInUserFromCache(key: string, value: string) {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      await this.client.LREM(key, 1, value);
+      logger.info(`User ${value} removed`);
+      const response: string[] = await this.client.LRANGE(key, 0, -1);
+      return response;
+    } catch (error) {
+      logger.log('error', 'GatewayService Cache removeLoggedInUserFromCache() method error:', error);
+      return [];
+    }
+  }
+}
+
+const gatewayCache = new GatewayCache();
+export default gatewayCache;
