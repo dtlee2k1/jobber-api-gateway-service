aa42b1fe3cb94308a4db94ce1a4048313f21c0c5
Le Duc Thai <leducthai2001@gmail.com>
2024-03-18 10:38:15 +0700

    handle chat service socket connections

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,5 +1,11 @@
+import { winstonLogger } from '@dtlee2k1/jobber-shared';
+import { envConfig } from '@gateway/config';
 import { GatewayCache } from '@gateway/redis/gateway.cache';
 import { Server, Socket } from 'socket.io';
+import { io, Socket as SocketClient } from 'socket.io-client';
+
+const logger = winstonLogger(`${envConfig.ELASTIC_SEARCH_URL}`, 'gatewaySocket', 'debug');
+let chatSocketClient: SocketClient;
 
 export class SocketIOAppHandler {
   private io: Server;
@@ -8,9 +14,12 @@ export class SocketIOAppHandler {
   constructor(io: Server) {
     this.io = io;
     this.gatewayCache = new GatewayCache();
+    this.chatSocketServiceIOConnections();
   }
 
   public listen() {
+    this.chatSocketServiceIOConnections();
+
     this.io.on('connection', async (socket: Socket) => {
       socket.on('getLoggedInUsers', async () => {
         const response: string[] = await this.gatewayCache.getLoggedInUsersFromCache('loggedInUsers');
@@ -30,6 +39,28 @@ export class SocketIOAppHandler {
       socket.on('category', async (category: string, username: string) => {
         await this.gatewayCache.saveUserSelectedGigCategory(`selectedCategories:${username}`, category);
       });
+    });
+  }
+
+  private chatSocketServiceIOConnections() {
+    chatSocketClient = io(`${envConfig.MESSAGE_BASE_URL}`, {
+      transports: ['websocket', 'polling'],
+      secure: true
+    });
+
+    chatSocketClient.on('connect', () => {
+      logger.info('ChatService socket connected');
+    });
+
+    chatSocketClient.on('disconnect', (reason: SocketClient.DisconnectReason) => {
+      logger.log('error', `ChatService socket connection error: ${reason}`);
+
+      chatSocketClient.connect();
+    });
+
+    chatSocketClient.on('connect_error', (error: Error) => {
+      logger.log('error', 'ChatService socket connection error:', error);
+      chatSocketClient.connect();
     });
   }
 }
