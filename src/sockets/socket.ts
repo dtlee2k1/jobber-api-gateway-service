fb0af2646690e9c1a9f327fb013387b3223c2c45
Le Duc Thai <leducthai2001@gmail.com>
2024-03-18 21:47:36 +0700

    handle custom events socketIO

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,18 +1,18 @@
-import { winstonLogger } from '@dtlee2k1/jobber-shared';
+import { IMessageDocument, winstonLogger } from '@dtlee2k1/jobber-shared';
 import { envConfig } from '@gateway/config';
 import { GatewayCache } from '@gateway/redis/gateway.cache';
 import { Server, Socket } from 'socket.io';
 import { io, Socket as SocketClient } from 'socket.io-client';
 
 const logger = winstonLogger(`${envConfig.ELASTIC_SEARCH_URL}`, 'gatewaySocket', 'debug');
-let chatSocketClient: SocketClient;
+let chatSocketClient: SocketClient; // socket between Gateway service (client) and Chat service (server)
 
 export class SocketIOAppHandler {
   private io: Server;
   private gatewayCache: GatewayCache;
 
   constructor(io: Server) {
-    this.io = io;
+    this.io = io; // socket between Frontend (client) and Gateway service (server)
     this.gatewayCache = new GatewayCache();
     this.chatSocketServiceIOConnections();
   }
@@ -61,6 +61,15 @@ export class SocketIOAppHandler {
     chatSocketClient.on('connect_error', (error: Error) => {
       logger.log('error', 'ChatService socket connection error:', error);
       chatSocketClient.connect();
+    });
+
+    // custom events
+    chatSocketClient.on('message_received', (data: IMessageDocument) => {
+      this.io.emit('message_received', data);
+    });
+
+    chatSocketClient.on('message_updated', (data: IMessageDocument) => {
+      this.io.emit('message_updated', data);
     });
   }
 }
